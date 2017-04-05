1a38ad72d43d831968c987ce58666a52f27f1c51
RyotaHirano <hirano@tsumikiinc.com>
2017-04-05 15:55:23 +0900

    add build files

--- src/store/index.js
+++ src/store/index.js
@@ -26,7 +26,6 @@ const store = new Vuex.Store({
             state.allowShowVideos = false;
             return data.items.length > 0 ? state.videos = data.items : false ;
           })
-        })
     },
     resetFetchVideos(state) {
       state.videos = []
