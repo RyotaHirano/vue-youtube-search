b5bf8a845330313af8fead3f9a85ec6eb0e35747
RyotaHirano <hirano@tsumikiinc.com>
2017-04-05 15:54:45 +0900

    update youtube API key

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 import 'whatwg-fetch'
 
 const API_YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet';
-const API_YOUTUBE_KEY = 'AIzaSyDt1LGyISVebRRJqw7lbe5UA_9ROF48v54';
+const API_YOUTUBE_KEY = 'AIzaSyD3UHH4HeY7ki2njecykZ2xMx9xToqgYNM';
 
 Vue.use(Vuex)
 
