ae112c938da56b467e30c3a2c22aebb3f90e7813
RyotaHirano <hirano@tsumikiinc.com>
2017-04-05 12:04:31 +0900

    add fadein effect for videos

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="c-list-item">
-    <div class="c-movie"
-    >
+  <div class="c-list-item"
+       :class="listItemClassObject"
+  >
+    <div class="c-movie">
       <h2>{{video.title}}</h2>
       <iframe width="560" height="315" :src="youtubeUrl" v-on:load="videoLoaded()" frameborder="0" allowfullscreen></iframe>
     </div>
@@ -18,12 +19,17 @@
       youtubeUrl: function() {
         const base = 'https://www.youtube.com/embed/';
         return `${base}${this.id}`
+      },
+      listItemClassObject: function() {
+        return {
+          'fade-in': this.$store.state.allowShowVideos
+        }
       }
     },
     methods: {
       videoLoaded: function() {
         this.$store.dispatch('updateLoadedVideoNum');
-        parseInt(this.$store.state.loadedVideoNum, 10) === parseInt(this.$store.state.videos.length, 10) ? this.$store.dispatch('allowShowVideos') : false ;
+        return parseInt(this.$store.state.loadedVideoNum, 10) === parseInt(this.$store.state.videos.length, 10) ? this.$store.dispatch('allowShowVideos') : false ;
       }
     }
   }
@@ -31,6 +37,9 @@
 
 <style lang="scss" scoped>
   .c-list-item {
+    opacity: 0;
+    transition: none;
+    transition-delay: 0s;
     & + .c-list-item {
       margin-top: 20px;
     }
@@ -40,6 +49,31 @@
     h2 {
       font-size: 20px;
       margin: 0;
+    }
+  }
+
+  /* transition */
+  .fade-in {
+    opacity: 1;
+    transition: opacity .5s ease;
+
+    &[data-index="0"] {
+      transition-delay: 0s;
+    }
+    &[data-index="1"] {
+      transition-delay: .1s;
+    }
+    &[data-index="2"] {
+      transition-delay: .2s;
+    }
+    &[data-index="3"] {
+      transition-delay: .3s;
+    }
+    &[data-index="4"] {
+      transition-delay: .4s;
+    }
+    &[data-index="5"] {
+      transition-delay: .5s;
     }
   }
 </style>
--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,23 +1,24 @@
 <template>
-  <transition-group
-    name="slide-fade"
-    tag="div"
-    class="c-list-items"
-    v-if="this.$store.state.allowShowVideos"
-    v-bind:css="false"
-    v-on:before-enter="beforeEnter"
-    v-on:enter="show"
-    v-on:leave="hide"
-  >
-    <list-item
-      v-for="(video, key) in fetchVideos"
-      :video="video.snippet"
-      :id="video.id.videoId"
-      :key="key"
-      :data-index="key"
+  <div>
+    <div
+      class="c-list-items"
     >
-    </list-item>
-  </transition-group>
+      <list-item
+        v-for="(video, key) in fetchVideos"
+        :video="video.snippet"
+        :id="video.id.videoId"
+        :key="key"
+        :data-index="key"
+      >
+      </list-item>
+    </div>
+    <div
+      v-show="!allowShowVideos"
+    >
+      <p>Loading...</p>
+    </div>
+  </div>
+
 </template>
 
 <script>
@@ -31,35 +32,9 @@
     computed: {
       fetchVideos: function() {
         return this.$store.state.videos
-      }
-    },
-    methods: {
+      },
       allowShowVideos: function() {
-        console.log('test');
         return this.$store.state.allowShowVideos
-      },
-      beforeEnter: function(el) {
-        el.style.opacity = 0
-      },
-      show: function(el, done) {
-        const delay = el.dataset.index * 150;
-        setTimeout(function () {
-          Velocity(
-            el,
-            { opacity: 1 },
-            { complete: done }
-          )
-        }, delay)
-      },
-      hide: function(el, done) {
-        const delay = el.dataset.index * 150;
-        setTimeout(function () {
-          Velocity(
-            el,
-            { opacity: 0 },
-            { complete: done }
-          )
-        }, delay)
       }
     },
     components: {
