1274d11971bf66a17286310f6feccc8e135fbdfa
Ryota Hirano <hirano@tsumikiinc.com>
2017-04-15 08:49:52 +0900

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <header class="header">
+      <h1>Youtube Search</h1>
+    </header>
     <search-input></search-input>
     <router-view></router-view>
   </div>
@@ -26,5 +29,39 @@
 
   * {
     box-sizing: border-box;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    margin: 0;
+    padding: 0;
+  }
+
+  .header {
+    width: 100%;
+    max-width: 850px;
+    height: 40px;
+    margin: 10px auto;
+
+    h1 {
+      font-size: 24px;
+      line-height: 40px;
+      display: inline-block;
+      color: #eee;
+    }
+
+    @media screen and (max-width: 960px) {
+      max-width: none;
+      height: 30px;
+      padding: 0 15px;
+
+      h1 {
+        line-height: 30px;
+        font-size: 20px;
+      }
+    }
   }
 </style>
