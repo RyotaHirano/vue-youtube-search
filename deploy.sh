091f8fae3de3abd052b3e2ecade0c363fd22394b
Ryota Hirano <hirano@tsumikiinc.com>
2018-12-31 18:34:47 +0900

    [fix] deploy shell

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,6 @@ git config --global user.name  "CircleCI"
 git config --global user.email "circleci@gh-pages.com"
 git add .
 git commit -m '[ci skip] build GitHubPaged on circle ci'
-git push origin master -f
+git push origin HEAD -f
 
 echo "Deploy github page."
-
-cd -
