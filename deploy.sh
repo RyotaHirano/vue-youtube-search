8c27b508a109d603622bafa368313f398d26dd9e
RyotaHirano <kitchenkamome@gmail.com>
2020-05-13 19:46:57 +0900

    [update] add docs dir

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,14 @@ echo "Start: Deploy github page."
 
 # build
 yarn run build
+echo "yarn run build finish."
 rm -rf docs
+echo "remove docs finish."
 mkdir docs
+echo "create docs finish."
 chmod -R 755 docs
 cp -f -R dist/* docs
+echo "copy dist to docs finish."
 
 git config --global user.name  "CircleCI"
 git config --global user.email "circleci@gh-pages.com"
